8bfe176469875b43d3d97cb6e7958ec3010491bc
Redha-MOHD <202210328@stu.uob.edu.bh>
2024-12-11 07:52:07 +0300

    Initialize server with host and port settings

--- s.py
+++ s.py
@@ -9,3 +9,13 @@ PORT = 12346
 API_KEY = '7e298875cb264cee89ac50bb42c59a83'
 BASE_URL = 'https://newsapi.org/v2/'
 
+class NewsServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(5)
+        print("The server is ready for connection")
+
+    
