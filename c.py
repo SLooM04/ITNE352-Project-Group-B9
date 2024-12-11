bf05d6c53efc91d595d316456eaf4836e38c29c8
Salman <129712913+SLooM04@users.noreply.github.com>
2024-12-11 08:03:19 +0300

    adding def start

--- c.py
+++ c.py
@@ -7,3 +7,11 @@ class NewsClient:
         self.port = port
         self.client_socket = None
 
+    def start(self):
+        if not self.client_socket:
+            print("Socket is not connected.")
+            return
+
+        username = input("Enter your name: ")
+        self.client_socket.send(username.encode())
+        print(self.client_socket.recv(1024).decode())
