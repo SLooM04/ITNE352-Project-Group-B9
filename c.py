d5b6d4796f4be46df240d37ed7f738e59c3552bc
Salman <129712913+SLooM04@users.noreply.github.com>
2024-12-11 08:04:06 +0300

    adding Main Menu

--- c.py
+++ c.py
@@ -15,3 +15,25 @@ class NewsClient:
         username = input("Enter your name: ")
         self.client_socket.send(username.encode())
         print(self.client_socket.recv(1024).decode())
+        while True:
+            try:
+                print("\nMain Menu:")
+                print("1 - Search Headlines")
+                print("2 - List Sources")
+                print("3 - Quit")
+
+                choice = input("Choose an option: ").strip()
+
+                if choice == "1":
+                    self.headlines_menu()
+                elif choice == "2":
+                    self.sources_menu()
+                elif choice == "3":
+                    self.client_socket.send("quit".encode())
+                    print("Goodbye!")
+                    break
+                else:
+                    print("Invalid choice. Please try again.")
+            except KeyboardInterrupt:
+                print("\nProgram interrupted. Exiting...")
+                break       
